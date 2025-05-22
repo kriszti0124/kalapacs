d6798712156292fa5799b76a1c1219bd8e60deac
JankoPetra <91984558+JankoPetra@users.noreply.github.com>
2025-05-22 13:26:15 +0200

    Add files via upload

---
+++ Fuggohidak/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Fuggohidak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.txbDarab = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbHely = new System.Windows.Forms.TextBox();
+            this.txbOrszag = new System.Windows.Forms.TextBox();
+            this.txbHossz = new System.Windows.Forms.TextBox();
+            this.txbEv = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuKereses = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuKilepes = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(58, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(192, 173);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txbDarab);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(58, 258);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 106);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hidak száma";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 31);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(98, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "2000 előtt épült";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2000-ben vagy utána";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // txbDarab
+            // 
+            this.txbDarab.Location = new System.Drawing.Point(6, 74);
+            this.txbDarab.Name = "txbDarab";
+            this.txbDarab.Size = new System.Drawing.Size(100, 20);
+            this.txbDarab.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "darab";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(456, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hely";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ország";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(456, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Hossz";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(456, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Év";
+            // 
+            // txbHely
+            // 
+            this.txbHely.Location = new System.Drawing.Point(517, 40);
+            this.txbHely.Name = "txbHely";
+            this.txbHely.Size = new System.Drawing.Size(100, 20);
+            this.txbHely.TabIndex = 6;
+            // 
+            // txbOrszag
+            // 
+            this.txbOrszag.Location = new System.Drawing.Point(517, 97);
+            this.txbOrszag.Name = "txbOrszag";
+            this.txbOrszag.Size = new System.Drawing.Size(100, 20);
+            this.txbOrszag.TabIndex = 7;
+            // 
+            // txbHossz
+            // 
+            this.txbHossz.Location = new System.Drawing.Point(517, 151);
+            this.txbHossz.Name = "txbHossz";
+            this.txbHossz.Size = new System.Drawing.Size(100, 20);
+            this.txbHossz.TabIndex = 8;
+            // 
+            // txbEv
+            // 
+            this.txbEv.Location = new System.Drawing.Point(517, 206);
+            this.txbEv.Name = "txbEv";
+            this.txbEv.Size = new System.Drawing.Size(100, 20);
+            this.txbEv.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Kilépés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuKereses,
+            this.menuKilepes});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuKereses
+            // 
+            this.menuKereses.Name = "menuKereses";
+            this.menuKereses.Size = new System.Drawing.Size(58, 20);
+            this.menuKereses.Text = "Keresés";
+            // 
+            // menuKilepes
+            // 
+            this.menuKilepes.Name = "menuKilepes";
+            this.menuKilepes.Size = new System.Drawing.Size(56, 20);
+            this.menuKilepes.Text = "Kilépés";
+            this.menuKilepes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txbEv);
+            this.Controls.Add(this.txbHossz);
+            this.Controls.Add(this.txbOrszag);
+            this.Controls.Add(this.txbHely);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Függőhidak";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbDarab;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbHely;
+        private System.Windows.Forms.TextBox txbOrszag;
+        private System.Windows.Forms.TextBox txbHossz;
+        private System.Windows.Forms.TextBox txbEv;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuKereses;
+        private System.Windows.Forms.ToolStripMenuItem menuKilepes;
+    }
+}
+
---
+++ Fuggohidak/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fuggohidak
+{
+    public partial class Form1 : Form
+    {
+        public static List<Fuggohid> lista = new List<Fuggohid>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("fuggohidak.csv");
+            string elsosor = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                Fuggohid sor = new Fuggohid(sr.ReadLine());
+                lista.Add(sor);
+                listBox1.Items.Add(sor.hid);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txbHely.Text = lista[listBox1.SelectedIndex].hely;
+            txbOrszag.Text = lista[listBox1.SelectedIndex].orszag;
+            txbHossz.Text = lista[listBox1.SelectedIndex].hossz.ToString();
+            txbEv.Text = lista[listBox1.SelectedIndex].atadas;
+        }
+    }
+
+    public class Fuggohid
+    {
+        public int helyezes { get; set; }
+        public string hid { get; set; }
+        public string hely { get; set; }
+        public string orszag { get; set; }
+        public int hossz { get; set; }
+        public string atadas { get; set; }
+
+        public Fuggohid(string sor)
+        {
+            string[] d = sor.Split('\t');
+            helyezes = Convert.ToInt32(d[0]);
+            hid = d[1];
+            hely = d[2];
+            orszag = d[3];
+            hossz = Convert.ToInt32(d[4]);
+            atadas = d[5];
+        }
+    }
+}
